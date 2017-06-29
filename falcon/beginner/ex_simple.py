f3668bb7d636587b03c706205a6b89b7e1aa19d2
davide <davide@localhost.localdomain>
2017-06-30 01:08:09 +0200

    falcon

---
+++ falcon/beginner/ex_simple.py
@@ -0,0 +1,19 @@
+""" easy """
+import falcon
+import json
+ 
+class QuoteResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': 'I\'ve always been more interested in the future than in the past.',
+            'author': 'Grace Hopper'
+        }
+
+        resp.body = json.dumps(quote)
+ 
+api = falcon.API()
+api.add_route('/quote', QuoteResource())
+
+# execute on terminal command line:
+# waitress-serve --port=8000 ex_simple:api
